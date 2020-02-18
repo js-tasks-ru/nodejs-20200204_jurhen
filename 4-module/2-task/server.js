6db82b783dc17d862b64f07871c0cc78e9d4a2ec
jurhen <jurhen2061@gmail.com>
2020-02-18 19:15:27 +0300

    test solution 4-module 2-task v.3

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -7,7 +7,6 @@ const LimitSizeStream = require('./LimitSizeStream');
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  let errorCreateFile = false;
   const pathname = url.parse(req.url).pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
@@ -17,7 +16,7 @@ server.on('request', (req, res) => {
   });
 
   if (pathname.includes('/')) {
-    res.writeHead(400);
+    res.statusCode = 400;
     res.end('Bad request');
     return;
   }
@@ -29,6 +28,7 @@ server.on('request', (req, res) => {
       req.on('aborted', () => {
         fs.unlink(filepath, (err) => {
           if (err) throw new Error(err);
+          writeStream.destroy();
         });
       });
 
@@ -36,7 +36,8 @@ server.on('request', (req, res) => {
         fs.unlink(filepath, (err) => {
           errorCreateFile = true;
           if (err) throw err;
-          res.writeHead(413);
+          res.statusCode = 413;
+          writeStream.destroy();
           res.end('File is too big!');
           return;
         });
@@ -44,14 +45,14 @@ server.on('request', (req, res) => {
 
       writeStream.on('error', (err) => {
         errorCreateFile = true;
-        res.writeHead(409);
+        res.statusCode = 409;
         res.end('File already exists');
         return;
       });
 
       writeStream.on('close', () => {
         if (!errorCreateFile) {
-          res.writeHead(200);
+          res.statusCode = 200;
           res.end('sucessfull!');
         }
       });
