c0d689f6721561c7c87b964ff7afc444073be4a5
jurhen <jurhen2061@gmail.com>
2020-02-18 21:01:52 +0300

    add solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -9,9 +10,32 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  if (pathname.includes('/')) {
+    res.writeHead(400);
+    res.end('Bad request');
+    return;
+  }
+
   switch (req.method) {
     case 'DELETE':
+      fs.access(filepath, fs.constants.F_OK, (err) => {
+        if (err || req.url === '/') {
+          res.writeHead(404);
+          res.end('Not available');
+          return;
+        }
 
+        res.on('error', (err) => {
+          res.writeHead(500);
+          res.end(err);
+        });
+
+        fs.unlink(filepath, (err) => {
+          if (err) throw new Error();
+          res.writeHead(200);
+          res.end('successfull deleted');
+        });
+      });
       break;
 
     default:
