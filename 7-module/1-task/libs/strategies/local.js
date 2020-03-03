a5dd5ea3a9d9d713272a3aadbb8c6c192d24552d
jurhen <jurhen2061@gmail.com>
2020-03-03 20:55:30 +0300

    add solution 7 module 1 task

--- 7-module/1-task/libs/strategies/local.js
+++ 7-module/1-task/libs/strategies/local.js
@@ -1,8 +1,22 @@
+/* eslint-disable indent */
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
-    {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+  {usernameField: 'email', session: false},
+  async function(email, password, done) {
+    const user = await User.findOne({email});
+    if (!user) {
+      done(null, false, 'Нет такого пользователя');
+      return;
     }
+
+    const res = await user.checkPassword(password);
+    if (!res) {
+      done(null, false, 'Неверный пароль');
+      return;
+    }
+
+    done(null, user);
+  },
 );
